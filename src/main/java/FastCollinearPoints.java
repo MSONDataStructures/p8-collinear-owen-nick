96a6599912519d188643226031ce92d3a30df09e
Owen Hallock <owenhallock29@gmail.com>
2025-02-27 08:08:09 -0500

    Pushed Nick's FastCollinearPoints.java code

--- src/main/java/FastCollinearPoints.java
+++ src/main/java/FastCollinearPoints.java
@@ -1,29 +1,54 @@
-/*************************************************************************
- *   Given a point p, the following method determines
- *   whether p participates in a set of 4 or more collinear points.
- *   Think of p as the origin.
- *   - For each other point q, determine the slope it makes with p.
- *   - Sort the points according to the slopes they makes with p.
- *   - Check if any 3 (or more) adjacent points in 
- *      the sorted order have equal slopes with respect to p. 
- *      If so, these points, together with p, are collinear.
- *************************************************************************/
+//Written by Nick Wong
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.*;
+
 public class FastCollinearPoints {
 
+    private final List<LineSegment> lineSegments = new ArrayList<>();
+
     public FastCollinearPoints(Point[] points) {
-        // finds all line segments containing 4 points
-        // TODO: YOUR CODE HERE
+
+        for (Point p : points) {
+            Point[] otherPoints = new Point[points.length - 1];
+            int index = 0;
+            for (Point q : points) {
+                if (!q.equals(p)) {
+                    otherPoints[index++] = q;
+                }
+            }
+
+            sort(otherPoints, p.slopeOrder());
+
+            int j = 0;
+            while (j < otherPoints.length) {
+                List<Point> collinear = new ArrayList<>();
+                collinear.add(p);
+
+                double slope = p.slopeTo(otherPoints[j]);
+                while (j < otherPoints.length && p.slopeTo(otherPoints[j]) == slope) {
+                    collinear.add(otherPoints[j]);
+                    j++;
+                }
+
+
+                if (collinear.size() >= 4) {
+                    collinear.sort(null);
+                    if (p.compareTo(collinear.get(0)) == 0) {
+                        lineSegments.add(new LineSegment(collinear.get(0), collinear.get(collinear.size() - 1)));
+                    }
+                }
+            }
+        }
     }
 
     public int numberOfSegments() {
-        // the number of line segments
-        // TODO: YOUR CODE HERE
-        return 0;
+        return lineSegments.size();
     }
 
     public LineSegment[] segments() {
-        // the line segments
-        // TODO: YOUR CODE HERE
-        return null;
+        return lineSegments.toArray(new LineSegment[0]);
     }
 }
